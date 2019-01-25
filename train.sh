0c007d693ef20ed0168b8b766e58835af5e8eebf
alex18212010045 <45620445+alex18212010045@users.noreply.github.com>
2019-01-25 11:57:55 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -3,4 +3,4 @@ pip2 install --index https://pypi.mirrors.ustc.edu.cn/simple/ --upgrade pip
 pip2 install --index https://pypi.mirrors.ustc.edu.cn/simple/ easydict
 pip2 install --index https://pypi.mirrors.ustc.edu.cn/simple/ pyyaml
 pip install tensorboardX
-python2 tools/cls_dropblock_imagenet.py --cfg cfgs/imagenet/resdropnet50_1x64d-Copy1.yml
+python2 tools/cls_dropblock_imagenet.py --cfg cfgs/imagenet/resdropnet50_1x64d-Copy2.yml
